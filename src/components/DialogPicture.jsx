de07dba38d7df3f75662a5ee5d6f7e9daf368895
ajayxsaini <153013883+ajayxsaini@users.noreply.github.com>
2025-12-13 16:19:01 +0000

    added a feature

--- src/components/DialogPicture.jsx
+++ src/components/DialogPicture.jsx
@@ -1,6 +1,6 @@
 const DialogPicture = () => {
   return (
-    <div className=" bg-lime-800 absolute w-2xl right-10 mt-4 " >
+    <div className=" bg-transparent absolute h-1/2 w-2xl right-10 mt-4 z-0 border-2 border-gray-400 box-border " >
       <div>
         <p>This is how we do it</p>
       </div>
