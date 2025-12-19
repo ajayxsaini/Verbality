4cabc56ebaaa82dd3807322eb65c8019aa4ec528
ajayxsaini <153013883+ajayxsaini@users.noreply.github.com>
2025-12-19 07:59:21 +0000

    made some changes

--- src/components/DialogPicture.jsx
+++ src/components/DialogPicture.jsx
@@ -1,8 +1,10 @@
 const DialogPicture = ({show}) => {
   return (
-    <div className= {`bg-transparent absolute h-1/2 w-2xl right-10 mt-4 z-0 border-2 border-gray-400 box-border ${show ? "opacity-100" : "opacity-0"} `  } >
-      <div>
-        <p>This is how we do it</p>
+    <div className= {`bg-transparent absolute  h-1/2 w-2xl right-10 mt-4 z-0 border-2 border-gray-400 box-border ${show ? "opacity-100" : "opacity-0"} `  } >
+      <div className="flex h-full w-full justify-center items-center">
+        <div>
+          <img src="public/image.png" alt="" />
+        </div>
       </div>
     </div>
   )
